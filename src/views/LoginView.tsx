234f11423d48bd35e30aa481180279c90bde5d75
cristian <cristi.jigau@gmail.com>
2025-09-18 00:10:52 +0300

    initial setup

---
+++ src/views/LoginView.tsx
@@ -0,0 +1,12 @@
+export const LoginView = () => {
+  return (
+    <div className="bg-red-500 size-full flex">
+      <div className="bg-amber-300 w-[70%]">Login Image</div>
+      <div className="bg-pink-500 flex-1">
+        <div>
+          <h1 className="text-3xl font-bold">Hello world!</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
